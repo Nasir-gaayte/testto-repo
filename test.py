48851800739ee6698a9cee9fc1fa4765c477aac6
nasir gaayte <slowboyz12@gmail.com>
2022-07-14 17:10:33 +0300

    am gona change it

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
+from tkinter import *
 
+
+root = Tk()
+root.geometry("500x500")
+
+
+
+
+
+
+l1= Label(root, text="I do repo live kkkk" ,font=("to",25))
+l1.pack()
+
+but1= Button(root,text="cleck")
+but1.pack()
+
+but2= Button(root,text="cleck222")
+but2.pack()
+
+root.mainloop()
